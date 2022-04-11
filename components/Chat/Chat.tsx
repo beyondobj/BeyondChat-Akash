0d6b9349b84033b2371ae00eedd8d566b271238c
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-04-11 21:45:24 +0000

    update to .gitignore

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -211,3 +211,95 @@ export const Chat: FC<Props> = memo(
                   </div>
                 </>
               ) : (
+                <>
+                  <div className="flex items-center justify-center space-x-5 border-b border-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#333d3d3d] dark:text-neutral-200">
+                    <div className="md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+                      <AkashChatLogo />
+                    </div>
+                  </div>
+                  <div className="flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#333d3d3d] dark:text-neutral-200">
+                    {t('Model')}: {conversation.model.name}
+                    <button
+                      className="ml-2 cursor-pointer hover:opacity-50"
+                      onClick={handleSettings}
+                    >
+                      <IconSettings size={18} />
+                    </button>
+                    <button
+                      className="ml-2 cursor-pointer hover:opacity-50"
+                      onClick={onClearAll}
+                    >
+                      <IconClearAll size={18} />
+                    </button>
+                  </div>
+                  {showSettings && (
+                    <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+                      <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
+                        <ModelSelect
+                          model={conversation.model}
+                          models={models}
+                          defaultModelId={defaultModelId}
+                          onModelChange={(model) =>
+                            onUpdateConversation(conversation, {
+                              key: 'model',
+                              value: model,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                  )}
+
+                  {conversation.messages.map((message, index) => (
+                    <ChatMessage
+                      key={index}
+                      message={message}
+                      messageIndex={index}
+                      onEditMessage={onEditMessage}
+                    />
+                  ))}
+
+                  {loading && <ChatLoader />}
+
+                  <div
+                    className="h-[162px] bg-white dark:bg-[#401c1c1c]"
+                    ref={messagesEndRef}
+                  />
+                </>
+              )}
+            </div>
+
+            <ChatInput
+              stopConversationRef={stopConversationRef}
+              textareaRef={textareaRef}
+              messageIsStreaming={messageIsStreaming}
+              conversationIsEmpty={conversation.messages.length === 0}
+              model={conversation.model}
+              prompts={prompts}
+              onSend={(message, plugin) => {
+                setCurrentMessage(message);
+                onSend(message, 0, plugin);
+              }}
+              onRegenerate={() => {
+                if (currentMessage) {
+                  onSend(currentMessage, 2, null);
+                }
+              }}
+            />
+          </>
+        )}
+        {showScrollDownButton && (
+          <div className="absolute bottom-0 right-0 mb-4 mr-4 pb-20">
+            <button
+              className="flex h-7 w-7 items-center justify-center rounded-full bg-neutral-300 text-gray-800 shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-neutral-200"
+              onClick={handleScrollDown}
+            >
+              <IconArrowDown size={18} />
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  },
+);
+Chat.displayName = 'Chat';
---
+++ components/Chat/ChatInput.tsx
@@ -0,0 +1,89 @@
+
+import { Message } from '@/types/chat';
+import { LLM } from '@/types/llms';
+import { Prompt } from '@/types/prompt';
+import { IconArrowUp, IconPlayerStop, IconRepeat } from '@tabler/icons-react';
+import { useTranslation } from 'next-i18next';
+import {
+  FC,
+  KeyboardEvent,
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { PromptList } from './PromptList';
+import { VariableModal } from './VariableModal';
+import { Spinner } from '../Global/Spinner';
+
+interface Props {
+  messageIsStreaming: boolean;
+  model: LLM;
+  conversationIsEmpty: boolean;
+  prompts: Prompt[];
+  onSend: (message: Message, plugin: Plugin | null) => void;
+  onRegenerate: () => void;
+  stopConversationRef: MutableRefObject<boolean>;
+  textareaRef: MutableRefObject<HTMLTextAreaElement | null>;
+}
+
+export const ChatInput: FC<Props> = ({
+  messageIsStreaming,
+  model,
+  conversationIsEmpty,
+  prompts,
+  onSend,
+  onRegenerate,
+  stopConversationRef,
+  textareaRef,
+}) => {
+  const { t } = useTranslation('chat');
+
+  const [content, setContent] = useState<string>();
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const [showPromptList, setShowPromptList] = useState(false);
+  const [activePromptIndex, setActivePromptIndex] = useState(0);
+  const [promptInputValue, setPromptInputValue] = useState('');
+  const [variables, setVariables] = useState<string[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const promptListRef = useRef<HTMLUListElement | null>(null);
+
+  const filteredPrompts = prompts.filter((prompt) =>
+    prompt.name.toLowerCase().includes(promptInputValue.toLowerCase()),
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    const maxLength = model.maxLength;
+
+    if (value.length > maxLength) {
+      alert(
+        t(
+          `Message limit is {{maxLength}} characters. You have entered {{valueLength}} characters.`,
+          { maxLength, valueLength: value.length },
+        ),
+      );
+      return;
+    }
+
+    setContent(value);
+    updatePromptListVisibility(value);
+  };
+
+  const handleSend = () => {
+    if (messageIsStreaming) {
+      return;
+    }
+
+    if (!content) {
+      alert(t('Please enter a message'));
+      return;
+    }
+
+    onSend({ role: 'user', content }, null);
+    setContent('');
+
+    if (window.innerWidth < 640 && textareaRef && textareaRef.current) {
+      textareaRef.current.blur();
