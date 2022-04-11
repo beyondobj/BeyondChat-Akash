91e76975f1580cc4aa08e9ed40c0db1f27782f65
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-04-11 11:59:37 +0000

    Javadocs.

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -19,3 +19,195 @@ import {
 } from 'react';
 import { Spinner } from '../Global/Spinner';
 import { ChatInput } from './ChatInput';
+import { ChatLoader } from './ChatLoader';
+import { ChatMessage } from './ChatMessage';
+import { ErrorMessageDiv } from './ErrorMessageDiv';
+import { ModelSelect } from './ModelSelect';
+import { SystemPrompt } from './SystemPrompt';
+import { AkashChatLogo } from '../Logos/akash-chat-logo';
+
+interface Props {
+  conversation: Conversation;
+  models: LLM[];
+  apiKey: string;
+  serverSideApiKeyIsSet: boolean;
+  defaultModelId: LLMID;
+  messageIsStreaming: boolean;
+  modelError: ErrorMessage | null;
+  loading: boolean;
+  prompts: Prompt[];
+  onSend: (
+    message: Message,
+    deleteCount: number,
+    plugin: Plugin | null,
+  ) => void;
+  onUpdateConversation: (
+    conversation: Conversation,
+    data: KeyValuePair,
+  ) => void;
+  onEditMessage: (message: Message, messageIndex: number) => void;
+  stopConversationRef: MutableRefObject<boolean>;
+}
+
+export const Chat: FC<Props> = memo(
+  ({
+    conversation,
+    models,
+    apiKey,
+    serverSideApiKeyIsSet,
+    defaultModelId,
+    messageIsStreaming,
+    modelError,
+    loading,
+    prompts,
+    onSend,
+    onUpdateConversation,
+    onEditMessage,
+    stopConversationRef,
+  }) => {
+    const { t } = useTranslation('chat');
+    const [currentMessage, setCurrentMessage] = useState<Message>();
+    const [autoScrollEnabled, setAutoScrollEnabled] = useState<boolean>(true);
+    const [showSettings, setShowSettings] = useState<boolean>(false);
+    const [showScrollDownButton, setShowScrollDownButton] =
+      useState<boolean>(false);
+
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const chatContainerRef = useRef<HTMLDivElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const scrollToBottom = useCallback(() => {
+      if (autoScrollEnabled) {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+        textareaRef.current?.focus();
+      }
+    }, [autoScrollEnabled]);
+
+    const handleScroll = () => {
+      if (chatContainerRef.current) {
+        const { scrollTop, scrollHeight, clientHeight } =
+          chatContainerRef.current;
+        const bottomTolerance = 30;
+
+        if (scrollTop + clientHeight < scrollHeight - bottomTolerance) {
+          setAutoScrollEnabled(false);
+          setShowScrollDownButton(true);
+        } else {
+          setAutoScrollEnabled(true);
+          setShowScrollDownButton(false);
+        }
+      }
+    };
+
+    const handleScrollDown = () => {
+      chatContainerRef.current?.scrollTo({
+        top: chatContainerRef.current.scrollHeight,
+        behavior: 'smooth',
+      });
+    };
+
+    const handleSettings = () => {
+      setShowSettings(!showSettings);
+    };
+
+    const onClearAll = () => {
+      if (confirm(t<string>('Are you sure you want to clear all messages?'))) {
+        onUpdateConversation(conversation, { key: 'messages', value: [] });
+      }
+    };
+
+    const scrollDown = () => {
+      if (autoScrollEnabled) {
+        messagesEndRef.current?.scrollIntoView(true);
+      }
+    };
+    const throttledScrollDown = throttle(scrollDown, 250);
+
+    useEffect(() => {
+      throttledScrollDown();
+      setCurrentMessage(
+        conversation.messages[conversation.messages.length - 2],
+      );
+    }, [conversation.messages, throttledScrollDown]);
+
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          setAutoScrollEnabled(entry.isIntersecting);
+          if (entry.isIntersecting) {
+            textareaRef.current?.focus();
+          }
+        },
+        {
+          root: null,
+          threshold: 0.5,
+        },
+      );
+      const messagesEndElement = messagesEndRef.current;
+      if (messagesEndElement) {
+        observer.observe(messagesEndElement);
+      }
+      return () => {
+        if (messagesEndElement) {
+          observer.unobserve(messagesEndElement);
+        }
+      };
+    }, [messagesEndRef]);
+
+    return (
+      <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#401c1c1c]">
+        {modelError ? (
+          <ErrorMessageDiv error={modelError} />
+        ) : (
+          <>
+            <div
+              className="max-h-full overflow-x-hidden"
+              ref={chatContainerRef}
+              onScroll={handleScroll}
+            >
+              {conversation.messages.length === 0 ? (
+                <>
+                  <div className="mx-auto flex w-[350px] flex-col space-y-10 pt-12 sm:w-[600px]">
+                    <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
+                      {models.length === 0 ? (
+                        <div>
+                          <Spinner size="16px" className="mx-auto" />
+                        </div>
+                      ) : (
+                        <div className="flex items-center justify-center space-x-5 border-b border-neutral-300 py-2 text-sm text-neutral-500 dark:border-none dark:text-neutral-200">
+                          <div className="md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+                            <AkashChatLogo />
+                          </div>
+                        </div>
+                      )}
+                    </div>
+
+                    {models.length > 0 && (
+                      <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
+                        <ModelSelect
+                          model={conversation.model}
+                          models={models}
+                          defaultModelId={defaultModelId}
+                          onModelChange={(model) =>
+                            onUpdateConversation(conversation, {
+                              key: 'model',
+                              value: model,
+                            })
+                          }
+                        />
+                        {/*}
+                        <SystemPrompt
+                          conversation={conversation}
+                          prompts={prompts}
+                          onChangePrompt={(prompt) =>
+                            onUpdateConversation(conversation, {
+                              key: 'prompt',
+                              value: prompt,
+                            })
+                          }
+                        /> */}
+                      </div>
+                    )}
+                  </div>
+                </>
+              ) : (
