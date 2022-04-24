846786840b7f8c1eedd9040c761cf46cf3a39d91
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-04-24 08:20:33 +0000

    change article

--- components/Chat/ModelSelect.tsx
+++ components/Chat/ModelSelect.tsx
@@ -1,3 +1,13 @@
 import { LLM, LLMID } from '@/types/llms';
 import { useTranslation } from 'next-i18next';
-import { IconExternalLink } from '@tabler/ic
+import { IconExternalLink } from '@tabler/icons-react';
+import { FC } from 'react';
+
+interface Props {
+  model: LLM;
+  models: LLM[];
+  defaultModelId: LLMID;
+  onModelChange: (model: LLM) => void;
+}
+
+export co
