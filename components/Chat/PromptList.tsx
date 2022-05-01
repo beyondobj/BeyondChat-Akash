d212d177582c9285b244656b4a5b49dcf30cfa61
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-05-01 16:11:17 +0000

    v0.04

--- components/Chat/PromptList.tsx
+++ components/Chat/PromptList.tsx
@@ -1,2 +1,8 @@
 import { Prompt } from '@/types/prompt';
-import { F
+import { FC, MutableRefObject } from 'react';
+
+interface Props {
+  prompts: Prompt[];
+  activePromptIndex: number;
+  onSelect: () => void;
+  onMouseOver: (index: 
