f8534e2ca2f9cd7559e373d876f629f9bfb6f46b
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-05-05 06:41:10 +0000

    Set executable bit

--- components/Chat/PromptList.tsx
+++ components/Chat/PromptList.tsx
@@ -26,4 +26,9 @@ export const PromptList: FC<Props> = ({
           key={prompt.id}
           className={`${
             index === activePromptIndex
-              ? 'bg-gray-200 dark:bg-[#242424] dark:text
+              ? 'bg-gray-200 dark:bg-[#242424] dark:text-black'
+              : ''
+          } cursor-pointer px-3 py-2 text-sm text-black dark:text-white`}
+          onClick={(e) => {
+            e.preventDefault();
+      
