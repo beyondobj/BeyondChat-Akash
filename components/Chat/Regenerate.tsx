97ccd31ec366a3e613c7f83fa5624581f08e1b53
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-05-07 22:42:46 +0000

    Update .gitignore file.

--- components/Chat/Regenerate.tsx
+++ components/Chat/Regenerate.tsx
@@ -1,1 +1,9 @@
-imp
+import { IconRefresh } from '@tabler/icons-react';
+import { useTranslation } from 'next-i18next';
+import { FC } from 'react';
+
+interface Props {
+  onRegenerate: () => void;
+}
+
+export co
