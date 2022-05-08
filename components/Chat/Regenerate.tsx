5d23df6ba8742afb3f4f69a75e0a78095ad81d55
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-05-08 20:04:47 +0000

    comment clean up

--- components/Chat/Regenerate.tsx
+++ components/Chat/Regenerate.tsx
@@ -9,4 +9,9 @@ interface Props {
 export const Regenerate: FC<Props> = ({ onRegenerate }) => {
   const { t } = useTranslation('chat');
   return (
-    <div className="fixed bottom-4 left-0 right-0 ml-auto mr-auto w-full px-2 sm:absolute sm:bot
+    <div className="fixed bottom-4 left-0 right-0 ml-auto mr-auto w-full px-2 sm:absolute sm:bottom-8 sm:left-[280px] sm:w-1/2 lg:left-[200px]">
+      <div className="mb-4 text-center text-red-500">
+        {t('Sorry, there was an error.')}
+      </div>
+      <button
+        c
