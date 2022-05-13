2fc5eecfb74af2ca1c31d931de02700b83ceedd9
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-05-13 16:50:00 +0000

    update order

--- components/Chat/SystemPrompt.tsx
+++ components/Chat/SystemPrompt.tsx
@@ -1,4 +1,14 @@
 import { Conversation } from '@/types/chat';
 import { LLMID } from '@/types/llms';
 import { Prompt } from '@/types/prompt';
-import { DEFAULT_SYSTEM_PROMPT } 
+import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
+import { useTranslation } from 'next-i18next';
+import {
+  FC,
+  KeyboardEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { 
