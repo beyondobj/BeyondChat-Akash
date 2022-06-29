345d23c04db353840ee0027f53dd94aba8a2cffb
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-06-29 20:52:18 +0000

    revised

--- components/Chatbar/ChatbarSettings.tsx
+++ components/Chatbar/ChatbarSettings.tsx
@@ -27,3 +27,33 @@ export const ChatbarSettings: FC<Props> = ({
   onApiKeyChange,
   onClearConversations,
   onExportConversations,
+  onImportConversations,
+}) => {
+  const { t } = useTranslation('sidebar');
+
+  return (
+    <div className="flex flex-col items-center space-y-1 border-t border-white/20 pt-1 text-sm">
+      {conversationsCount > 0 ? (
+        <ClearConversations onClearConversations={onClearConversations} />
+      ) : null}
+
+      <Import onImport={onImportConversations} />
+
+      <SidebarButton
+        text={t('Export data')}
+        icon={<IconFileExport size={18} />}
+        onClick={() => onExportConversations()}
+      />
+
+      <SidebarButton
+        text={lightMode === 'light' ? t('Dark mode') : t('Light mode')}
+        icon={
+          lightMode === 'light' ? <IconMoon size={18} /> : <IconSun size={18} />
+        }
+        onClick={() =>
+          onToggleLightMode(lightMode === 'light' ? 'dark' : 'light')
+        }
+      />
+    </div>
+  );
+};
---
+++ components/Chatbar/ClearConversations.tsx
@@ -0,0 +1,3 @@
+import { IconCheck, IconTrash, IconX } from '@tabler/icons-react';
+import { useTranslation } from 'next-i18next';
+import { FC, useState } from 'react';
