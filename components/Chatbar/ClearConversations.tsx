e853a5401bf091eaee9a08d4c1e4666a240aeafa
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-06-30 08:24:55 +0000

    tabs instead of spaces

--- components/Chatbar/ClearConversations.tsx
+++ components/Chatbar/ClearConversations.tsx
@@ -1,3 +1,10 @@
 import { IconCheck, IconTrash, IconX } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, useState } from 'react';
+import { SidebarButton } from '../Sidebar/SidebarButton';
+
+interface Props {
+  onClearConversations: () => void;
+}
+
+export const ClearConve
