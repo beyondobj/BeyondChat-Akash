92dc27d660c02068662314b241c916157139122e
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-07-10 09:18:46 +0000

    msm: kgsl: Cast perfcounter query count to an unsigned int

--- components/Chatbar/Conversations.tsx
+++ components/Chatbar/Conversations.tsx
@@ -27,4 +27,9 @@ export const Conversations: FC<Props> = ({
     <div className="flex w-full flex-col gap-1">
       {conversations
         .slice()
-    
+        .reverse()
+        .map((conversation, index) => (
+          <ConversationComponent
+            key={index}
+            selectedConversation={selectedConversation}
+      
