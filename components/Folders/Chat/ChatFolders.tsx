13f1df8cee06c1b659c4c4c55e7566320da61270
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-07-12 20:15:32 +0000

    added installation instructions

--- components/Folders/Chat/ChatFolders.tsx
+++ components/Folders/Chat/ChatFolders.tsx
@@ -1,1 +1,8 @@
-import { Conversation } from '@/types/ch
+import { Conversation } from '@/types/chat';
+import { KeyValuePair } from '@/types/data';
+import { Folder } from '@/types/folder';
+import { FC } from 'react';
+import { ChatFolder } from './ChatFolder';
+
+interface Props {
+  search
