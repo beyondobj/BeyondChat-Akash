42b671796f877284e26462c290280efdbbdb31f2
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-07-27 08:25:50 +0000

    typescripting

--- components/Folders/Prompt/PromptFolders.tsx
+++ components/Folders/Prompt/PromptFolders.tsx
@@ -1,4 +1,13 @@
 import { Folder } from '@/types/folder';
 import { Prompt } from '@/types/prompt';
 import { FC } from 'react';
-import { PromptFolder } from './Pro
+import { PromptFolder } from './PromptFolder';
+
+interface Props {
+  searchTerm: string;
+  prompts: Prompt[];
+  folders: Folder[];
+  onDeleteFolder: (folder: string) => void;
+  onUpdateFolder: (folder: string, name: string) => void;
+  // prompt props
+  onD
