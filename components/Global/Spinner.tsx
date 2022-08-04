9a303f7b2bcb3e4ae88d48f72e8511b9f31250d4
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-08-04 22:33:42 +0000

    xml fix

--- components/Global/Spinner.tsx
+++ components/Global/Spinner.tsx
@@ -5,4 +5,12 @@ interface Props {
   className?: string;
 }
 
-export const Spinner: FC<Props> = ({ size = '1em', classN
+export const Spinner: FC<Props> = ({ size = '1em', className = '' }) => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      s
