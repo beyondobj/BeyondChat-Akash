64aba83844e0b32a70f3c459dc208e31e286b7cb
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-08-05 21:09:26 +0000

    Update FSF's address.

--- components/Global/Spinner.tsx
+++ components/Global/Spinner.tsx
@@ -13,4 +13,10 @@ export const Spinner: FC<Props> = ({ size = '1em', className = '' }) => {
       strokeWidth="2"
       viewBox="0 0 24 24"
       strokeLinecap="round"
-      s
+      strokeLinejoin="round"
+      className={`animate-spin ${className}`}
+      height={size}
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="12" y1="2" x2="12" y
