584af50c40b1b887613bad13d71e295dbf94bc0e
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-08-07 14:17:09 +0000

    mobile fixes

--- components/Global/Spinner.tsx
+++ components/Global/Spinner.tsx
@@ -19,4 +19,7 @@ export const Spinner: FC<Props> = ({ size = '1em', className = '' }) => {
       width={size}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <line x1="12" y1="2" x2="12" y
+      <line x1="12" y1="2" x2="12" y2="6"></line>
+      <line x1="12" y1="18" x2="12" y2="22"></line>
+      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+      <line x1="16.24" y1="16.24" x2="19.07" y2=
