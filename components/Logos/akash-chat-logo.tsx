8e9b6a20df65f8a7b09403f22eee899ddb568024
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-08-08 10:37:53 +0000

    updated webpage

--- components/Logos/akash-chat-logo.tsx
+++ components/Logos/akash-chat-logo.tsx
@@ -2,4 +2,11 @@ export const AkashChatLogo = (props: any) => {
   return (
     <svg
       width={'100%'}
-    
+      height={'auto'}
+      viewBox="0 20 1046 179"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M103.453 131.282L128.998 176
