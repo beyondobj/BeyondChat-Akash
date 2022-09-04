18e9e0997d5469b1479b5090324ae46ae9ae46a4
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-09-04 11:06:43 +0000

    Fixing syntax errors.

--- components/Markdown/CodeBlock.tsx
+++ components/Markdown/CodeBlock.tsx
@@ -5,4 +5,8 @@ import {
 import { IconCheck, IconClipboard, IconDownload } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, memo, useState } from 'react';
-import { 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+interface Props {
+  language: st
