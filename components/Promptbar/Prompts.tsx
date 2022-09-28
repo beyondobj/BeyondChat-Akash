2020a39c26014dbc56344d19c374aa81d59cd454
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-09-28 21:58:31 +0000

    [ALPS02264561] platform: improve system stability

--- components/Promptbar/Prompts.tsx
+++ components/Promptbar/Prompts.tsx
@@ -1,3 +1,11 @@
 import { Prompt } from '@/types/prompt';
 import { FC } from 'react';
-import { PromptCo
+import { PromptComponent } from './Prompt';
+
+interface Props {
+  prompts: Prompt[];
+  onUpdatePrompt: (prompt: Prompt) => void;
+  onDeletePrompt: (prompt: Prompt) => void;
+}
+
+exp
