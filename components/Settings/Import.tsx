243beed89a9b194bb335f0c595833a44717979c1
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-10-02 08:22:39 +0000

    Uniformize code

--- components/Settings/Import.tsx
+++ components/Settings/Import.tsx
@@ -1,2 +1,8 @@
 import { SupportedExportFormats } from '@/types/export';
-import { IconFileImport } from '@ta
+import { IconFileImport } from '@tabler/icons-react';
+import { useTranslation } from 'next-i18next';
+import { FC } from 'react';
+import { SidebarButton } from '../Sidebar/SidebarButton';
+
+interface Props {
+ 
