e59e6f87c7f54cf1b2b5065cbc2ecf24bf96501f
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-10-10 07:40:09 +0000

    version 3.0.1

--- components/Settings/Key.tsx
+++ components/Settings/Key.tsx
@@ -1,1 +1,4 @@
-import { IconCheck, I
+import { IconCheck, IconKey, IconX } from '@tabler/icons-react';
+import { useTranslation } from 'next-i18next';
+import { FC, KeyboardEvent, useEffect, useRef, useState } from 'react';
+import { SidebarButton } f
