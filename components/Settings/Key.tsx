45ce75baee27d542c9f85c23d7b1bd84294d019b
beyondobj <161191651+beyondobj@users.noreply.github.com>
2022-10-12 14:22:42 +0000

    Added new problem

--- components/Settings/Key.tsx
+++ components/Settings/Key.tsx
@@ -1,4 +1,11 @@
 import { IconCheck, IconKey, IconX } from '@tabler/icons-react';
 import { useTranslation } from 'next-i18next';
 import { FC, KeyboardEvent, useEffect, useRef, useState } from 'react';
-import { SidebarButton } f
+import { SidebarButton } from '../Sidebar/SidebarButton';
+
+interface Props {
+  apiKey: string;
+  onApiKeyChange: (apiKey: string) => void;
+}
+
+export const Key: FC<Props> = ({ apiKey, onApiKeyChange 
