2da2eb37bc0a91abe3fdc81340061a7ee4079309
beyondobj <161191651+beyondobj@users.noreply.github.com>
2023-08-07 14:43:22 +0000

    Converting tabs to spaces

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
 import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
-import type 
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+
+const inter = Inter({ subsets: ['latin'] });
+
+fun
