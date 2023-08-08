134204b351210511b1076ecc2e4c9438d4b8b43f
beyondobj <161191651+beyondobj@users.noreply.github.com>
2023-08-08 17:36:21 +0000

    removed printf

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,2 +1,5 @@
 import { ChatBody, Message } from '@/types/chat';
-import { DEFAU
+import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
+import { OpenAIError, OpenAIStream } from '@/utils/server';
+import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
+import { Tiktoken, i
