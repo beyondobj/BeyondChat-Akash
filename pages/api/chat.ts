6dea709721d3be37f1730144ce7eff283723eb7a
beyondobj <161191651+beyondobj@users.noreply.github.com>
2023-08-08 18:17:02 +0000

    Add DOAP file

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -2,4 +2,9 @@ import { ChatBody, Message } from '@/types/chat';
 import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
 import { OpenAIError, OpenAIStream } from '@/utils/server';
 import tiktokenModel from '@dqbd/tiktoken/encoders/cl100k_base.json';
-import { Tiktoken, i
+import { Tiktoken, init } from '@dqbd/tiktoken/lite/init';
+// @ts-expect-error
+import wasm from '../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm?module';
+
+export const config = {
+  runtime:
