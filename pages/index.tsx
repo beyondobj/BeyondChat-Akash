0eb466caf831848148393c29b07a8dcc2e2d0088
beyondobj <161191651+beyondobj@users.noreply.github.com>
2023-08-25 16:29:33 +0000

    lala

--- pages/index.tsx
+++ pages/index.tsx
@@ -731,3 +731,53 @@ const Home: React.FC<HomeProps> = ({
                   className="absolute top-0 left-0 z-10 h-full w-full bg-black opacity-70 sm:hidden"
                 ></div>
               </div>
+            ) : (
+              <button
+                className="fixed top-2.5 left-4 z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:left-4 sm:h-8 sm:w-8 sm:text-neutral-700"
+                onClick={handleToggleChatbar}
+              >
+                <IconArrowBarRight />
+              </button>
+            )}
+            <div className="flex flex-1">
+              <Chat
+                conversation={selectedConversation}
+                messageIsStreaming={messageIsStreaming}
+                apiKey={apiKey}
+                serverSideApiKeyIsSet={serverSideApiKeyIsSet}
+                defaultModelId={defaultModelId}
+                modelError={modelError}
+                models={models}
+                loading={loading}
+                prompts={prompts}
+                onSend={handleSend}
+                onUpdateConversation={handleUpdateConversation}
+                onEditMessage={handleEditMessage}
+                stopConversationRef={stopConversationRef}
+              />
+            </div>
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const defaultModelId = process.env.DEFAULT_MODEL;
+
+  return {
+    props: {
+      serverSideApiKeyIsSet: !!process.env.API_KEY,
+      defaultModelId,
+      ...(await serverSideTranslations(locale ?? 'en', [
+        'common',
+        'chat',
+        'sidebar',
+        'markdown',
+        'promptbar',
+      ])),
+    },
+  };
+};
