2f38db3c216d800defae414258dd41a3da3c8492
beyondobj <161191651+beyondobj@users.noreply.github.com>
2024-02-10 18:44:44 +0000

    Update to new version

--- types/llms.ts
+++ types/llms.ts
@@ -8,3 +8,8 @@ export interface LLM {
 export enum LLMID {
   MISTRAL = 'mistral',
   MIXTRAL = 'mixtral',
+  DOLPHINMIXTRAL = 'dolphin-mixtral',
+  NOUSHERMESMIXTRAL = 'nous-hermes2-mixtral',
+}
+
+// in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported m
