d281bffbb675c42026263cb3586664e7d8fcf22d
beyondobj <161191651+beyondobj@users.noreply.github.com>
2024-02-26 10:38:10 +0000

    Improved code style

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.DEFAULT_SYSTEM_PROMPT ||
-  "You are a helpful and fri
+  "You are a helpful and friendly assistant. Follow the user's instructions carefully. Don't use emojis or slang. Provide accurate and helpful information.";
+
+export const API_HOST = process.env.API_HOST;
