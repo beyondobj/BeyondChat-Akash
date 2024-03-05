3df53247320cfd14a6755c90e908f61051d58f63
beyondobj <161191651+beyondobj@users.noreply.github.com>
2024-03-05 07:10:27 +0000

    aoe: do not call bdi_init after blk_alloc_queue

--- utils/app/prompts.ts
+++ utils/app/prompts.ts
@@ -1,4 +1,15 @@
 import { Prompt } from '@/types/prompt';
 
 export const updatePrompt = (updatedPrompt: Prompt, allPrompts: Prompt[]) => {
-  const updatedPrompts =
+  const updatedPrompts = allPrompts.map((c) => {
+    if (c.id === updatedPrompt.id) {
+      return updatedPrompt;
+    }
+
+    return c;
+  });
+
+  savePrompts(updatedPrompts);
+
+  return {
+    single: updatedPr
