713bf903674f7a850a33bce5d7ffb837b27c651a
beyondobj <161191651+beyondobj@users.noreply.github.com>
2024-03-10 12:14:04 +0000

    testing travis

--- utils/index.ts
+++ utils/index.ts
@@ -3,4 +3,12 @@ export function throttle<T extends (...args: any[]) => any>(
   limit: number,
 ): T {
   let lastFunc: ReturnType<typeof setTimeout>;
-  let las
+  let lastRan: number;
+
+  return ((...args) => {
+    if (!lastRan) {
+      func(...args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      
